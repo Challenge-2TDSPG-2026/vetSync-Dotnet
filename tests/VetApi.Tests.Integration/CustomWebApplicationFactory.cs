48a197a026a944ba83ddf1b82e8e43d1a8d7f27c
PedroBrum-DEV <Pbrum191@gmail.com>
2026-09-08 16:56:17 -0300

    test: cria projeto de testes de integracao com WebApplicationFactory e banco InMemory

---
+++ tests/VetApi.Tests.Integration/CustomWebApplicationFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using VetApi.Data;
+
+namespace VetApi.Tests.Integration;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    public string DatabaseName { get; } = Guid.NewGuid().ToString();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Testing");
+
+        builder.ConfigureServices(services =>
+        {
+            var descriptorsParaRemover = services
+                .Where(d => d.ServiceType == typeof(DbContextOptions<AppDbContext>)
+                         || d.ServiceType == typeof(AppDbContext)
+                         || (d.ServiceType.IsGenericType && d.ServiceType.GetGenericTypeDefinition() == typeof(DbContextOptions<>)))
+                .ToList();
+
+            foreach (var descriptor in descriptorsParaRemover)
+                services.Remove(descriptor);
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseInMemoryDatabase(DatabaseName));
+        });
+    }
+}
---
+++ tests/VetApi.Tests.Integration/IntegrationTestCollection.cs
@@ -0,0 +1,8 @@
+﻿using Xunit;
+
+namespace VetApi.Tests.Integration;
+
+[CollectionDefinition("Integration Tests")]
+public class IntegrationTestCollection : ICollectionFixture<CustomWebApplicationFactory>
+{
+}
