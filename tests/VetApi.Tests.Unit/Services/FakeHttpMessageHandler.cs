7102c088a8f3a2a73a660627336a79a396d3accf
PedroBrum-DEV <Pbrum191@gmail.com>
2026-09-08 16:53:49 -0300

    test: cria projeto de testes unitarios com fixtures e fake http handler

---
+++ tests/VetApi.Tests.Unit/Services/FakeHttpMessageHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+
+namespace VetApi.Tests.Unit.Services;
+
+public class FakeHttpMessageHandler : HttpMessageHandler
+{
+    private readonly HttpStatusCode _statusCode;
+    private readonly string _responseBody;
+    public HttpRequestMessage? UltimaRequisicao { get; private set; }
+    public string? UltimoCorpoRequisicao { get; private set; }
+
+    public FakeHttpMessageHandler(string responseBody, HttpStatusCode statusCode = HttpStatusCode.OK)
+    {
+        _responseBody = responseBody;
+        _statusCode = statusCode;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        UltimaRequisicao = request;
+        if (request.Content is not null)
+            UltimoCorpoRequisicao = await request.Content.ReadAsStringAsync(cancellationToken);
+
+        return new HttpResponseMessage(_statusCode)
+        {
+            Content = new StringContent(_responseBody)
+        };
+    }
+}
---
+++ tests/VetApi.Tests.Unit/Services/GeoLocationServiceFixture.cs
@@ -0,0 +1,14 @@
+﻿using VetApi.Services;
+using Xunit;
+
+namespace VetApi.Tests.Unit.Services;
+
+public class GeoLocationServiceFixture
+{
+    public IGeoLocationService Service { get; } = new GeoLocationService();
+}
+
+[CollectionDefinition("GeoLocationService Collection")]
+public class GeoLocationServiceCollection : ICollectionFixture<GeoLocationServiceFixture>
+{
+}
